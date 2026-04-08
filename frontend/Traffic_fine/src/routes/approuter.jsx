fa33c1bdca4b2ad21bb96cf7d14d40725f13eff1
Yasitha Miyuranga <yasithamiyuranga219@gmail.com>
2026-04-08 11:50:19 +0530

    ViolationDetails management page and update app routing configuration

--- frontend/Traffic_fine/src/routes/approuter.jsx
+++ frontend/Traffic_fine/src/routes/approuter.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Authrouter from './authrouter';
 import Welcomeouter from './welcomeouter';
 import DashboardRouter from './dashboardRouter';
-import PoliceRouter from './policeRouter';
+// import PoliceRouter from './policeRouter';
 
 
 import { Route, Routes } from 'react-router-dom';
@@ -14,7 +14,7 @@ const Approuter = () => {
       <Routes>
         <Route path="/auth/*" element={<Authrouter />} />
         <Route path="/dashboard/*" element={<DashboardRouter />} />
-        <Route path="/police/*" element={<PoliceRouter />} />
+        {/* <Route path="/police/*" element={<PoliceRouter />} /> */}
         <Route path="/*" element={<Welcomeouter />} />
       </Routes>
     </>
